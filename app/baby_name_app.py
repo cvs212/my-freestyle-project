6702ab4a0bd0f27217e6ec742252326d96a9e058
cvs212 <39269850+cvs212@users.noreply.github.com>
2018-06-22 16:44:41 -0400

    Added the 'name by letter' and writer functions
    Now writes to CSV files

--- app/baby_name_app.py
+++ app/baby_name_app.py
@@ -3,6 +3,7 @@ import requests
 import json
 import csv
 import os
+import itertools
 
 api_key = "ch121382676"
 
@@ -27,6 +28,46 @@ def name_meaning():
 
 #behind_the_name_url = f"https://www.behindthename.com/api/lookup.json?name={baby_name}&key={api_key}"
 
+def name_letter():
+    user_name_letter = input("You selected the Names by Letter Function. Please enter the letter by which you want to look up name information: ")
+    if user_name_letter.isalpha() == False or len(user_name_letter) > 1: #source for checking whether an input is a letter: https://stackoverflow.com/questions/18667410/how-can-i-check-if-a-string-only-contains-letters-in-python
+        print("The input that you entered isn't valid. Please try again.")
+        quit("Stopping the program. Feel free to start over.")
+    user_name_letter_gender = input("Cool. Now enter the gender of names that you're interested in: ")
+    if user_name_letter_gender == "Male" or user_name_letter_gender == "male":
+        matching_names = [names for names in babynames_boys if names["name"][0] == user_name_letter]
+        filename = "babynames_" + user_name_letter + "_" + user_name_letter_gender + ".csv"
+        for names in matching_names:
+            print("------------------------------")
+            print("Name: " + names["name"] + " | 2018 Rank: " + names["2018 rank"] + " | 2017 Rank: " + names["2017 rank"])
+            difference_in_ranks(names)
+            per_capita(names)
+            print("------------------------------")
+            filepath_2 = os.path.join(os.path.dirname(__file__), "data", filename)
+            with open(filepath_2, "w") as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=["2018 rank", "name", "2017 rank", "popularity", "gender", "syllables"])
+                writer.writeheader()
+                for names in matching_names:
+                    writer.writerow(names)
+    elif user_name_letter_gender == "Female" or user_name_letter_gender == "female":
+        matching_names = [names for names in babynames_girls if names["name"][0] == user_name_letter]
+        filename = "babynames_" + user_name_letter + "_" + user_name_letter_gender + ".csv"
+        for names in matching_names:
+            print("------------------------------")
+            print("Name: " + names["name"] + " | 2018 Rank: " + names["2018 rank"] + " | 2017 Rank: " + names["2017 rank"])
+            difference_in_ranks(names)
+            per_capita(names)
+            print("------------------------------")
+            filepath_2 = os.path.join(os.path.dirname(__file__), "data", filename)
+            with open(filepath_2, "w") as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=["2018 rank", "name", "2017 rank", "popularity", "gender", "syllables"])
+                writer.writeheader()
+                for names in matching_names:
+                    writer.writerow(names)
+    else:
+        print("You seemed to have made an error with the last input. Feel free to try again")
+        name_letter()
+
 
 
 #related names
@@ -36,6 +77,9 @@ def name_information():
     behind_the_name_related_names_url = f"https://www.behindthename.com/api/related.json?name={user_input_related_names}&usage=eng&key={api_key}"
     response_3 = requests.get(behind_the_name_related_names_url)
     response_json_2 = json.loads(response_3.text)
+    if "error" in response_3.text: #source: your demo on validation errors in class
+        print("The name you requested isn't in the system. Please try again.")
+        quit("Stopping the program. Feel free to start over.")
     related_names = response_json_2['names']
     print(f"These are the related names for {user_input_related_names.title()}: ")
     for names in related_names:
@@ -64,6 +108,13 @@ def read_names_from_file_girl(filename="babynames_girls.csv"):
     return babynames_girls
 #assert: len = 200
 
+def write_products_to_file(filename):
+    filepath_2 = os.path.join(os.path.dirname(__file__), "data", filename)
+    with open(filepath_2, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=["2018 rank", "name", "2017 rank", "popularity", "gender", "syllables"])
+        writer.writeheader()
+
+
 babynames_boys = read_names_from_file()
 
 babynames_girls = read_names_from_file_girl()
@@ -73,7 +124,7 @@ def name_rankings():
     if user_gender_preference == "Male" or user_gender_preference == "male":
         user_rankings_range = input("Great. Now enter the number of the last ranked name you'd like to see in your list: ")
         last_index = int(user_rankings_range)
-        list_babynames_boys = babynames_boys[0:last_index]
+        list_babynames_boys = babynames_boys[0:last_index] #source for index pull: https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-given-a-list-containing-it-in-python
         for name in list_babynames_boys:
             print("------------------------------")
             print("Name: " + name["name"] + " | 2018 Rank: " + name["2018 rank"] + " | 2017 Rank: " + name["2017 rank"])
@@ -112,4 +163,5 @@ def per_capita(name):
 
 #name_rankings()
 #name_meaning()
-name_information()
+#name_information()
+name_letter()
